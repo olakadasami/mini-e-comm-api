81b3ad74041905028a33ca08ba2764f28d696bc3
olakadasami <olakadasamuel@gmail.com>
2024-06-20 20:16:02 +0100

    chore: add authorization in cart controller

--- app/controllers/cart_controller.ts
+++ app/controllers/cart_controller.ts
@@ -1,5 +1,6 @@
 import Cart from '#models/cart'
 import CartItem from '#models/cart_item'
+import CartPolicy from '#policies/cart_policy'
 import { addNewCartItemValidator } from '#validators/cart'
 import type { HttpContext } from '@adonisjs/core/http'
 
@@ -9,11 +10,13 @@ export default class CartController {
    * @route GET ':userId'
    * @param {number} userId
    */
-  async show({ params, response }: HttpContext) {
+  async show({ params, response, bouncer }: HttpContext) {
     const cart = await Cart.query().where('user_id', params.userId).preload('items').first()
     if (!cart) {
       return response.status(404).json({ message: 'Cart not found' })
     }
+    await bouncer.with(CartPolicy).authorize('view', cart)
+
     return response.json(cart)
   }
 
@@ -22,10 +25,12 @@ export default class CartController {
    * @route POST 'add-item'
    * @body {userId, productId, quantity}
    */
-  async addItem({ request, response }: HttpContext) {
+  async addItem({ request, response, bouncer }: HttpContext) {
     const { userId, productId, quantity } = await request.validateUsing(addNewCartItemValidator)
 
     const cart = await Cart.firstOrCreate({ userId })
+
+    await bouncer.with(CartPolicy).authorize('addItem', cart)
 
     const cartItem = await CartItem.create({
       cartId: cart.id,
@@ -41,9 +46,12 @@ export default class CartController {
    * @route POST 'update-item/:id'
    * @param {number} id
    */
-  async updateItem({ params, request, response }: HttpContext) {
+  async updateItem({ params, request, response, bouncer }: HttpContext) {
     const cartItem = await CartItem.findOrFail(params.id)
     const { quantity } = request.only(['quantity'])
+
+    const cart = await Cart.findOrFail(cartItem.cartId)
+    await bouncer.with(CartPolicy).authorize('updateItem', cart)
 
     await cartItem.merge({ quantity }).save()
 
@@ -55,8 +63,11 @@ export default class CartController {
    * @description Remove one item from cart
    * @param {number} id
    */
-  async removeItem({ params, response }: HttpContext) {
+  async removeItem({ params, response, bouncer }: HttpContext) {
     const cartItem = await CartItem.findOrFail(params.id)
+
+    const cart = await Cart.findOrFail(cartItem.cartId)
+    await bouncer.with(CartPolicy).authorize('removeItem', cart)
 
     await cartItem.delete()
     return response.status(204).json(null)
@@ -67,8 +78,10 @@ export default class CartController {
    * @route DELETE 'clear/:userId'
    * @param{number} userId
    */
-  async clear({ params, response }: HttpContext) {
+  async clear({ params, response, bouncer }: HttpContext) {
     const cart = await Cart.findByOrFail('user_id', params.userId)
+
+    await bouncer.with(CartPolicy).authorize('clear', cart)
 
     await CartItem.query().where('cart_id', cart.id).delete()
     return response.status(204).json(null)
