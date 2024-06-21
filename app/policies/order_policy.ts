018dbe7a0a317541a1f09c5b25c1880d39e687d5
olakadasami <olakadasamuel@gmail.com>
2024-06-21 20:40:55 +0100

    chore: update order policy to false when status is shipped or delivered

--- app/policies/order_policy.ts
+++ app/policies/order_policy.ts
@@ -2,6 +2,7 @@ import User from '#models/user'
 import Order from '#models/order'
 import { AuthorizerResponse } from '@adonisjs/bouncer/types'
 import BasePolicy from './base_policy.js'
+import OrderStatus from '../enums/order_status.js'
 
 export default class OrderPolicy extends BasePolicy {
   view(user: User, order: Order): AuthorizerResponse {
@@ -9,10 +10,16 @@ export default class OrderPolicy extends BasePolicy {
   }
 
   update(user: User, order: Order): AuthorizerResponse {
+    if (order.status === OrderStatus.SHIPPED || order.status === OrderStatus.DELIVERED) {
+      return false
+    }
     return user.id === order.userId
   }
 
   destroy(user: User, order: Order): AuthorizerResponse {
+    if (order.status === OrderStatus.SHIPPED || order.status === OrderStatus.DELIVERED) {
+      return false
+    }
     return user.id === order.userId
   }
 }
