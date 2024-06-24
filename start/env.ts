a13c8d859b4aaf7bbccbd192185e0cb46355c3f6
olakadasami <olakadasamuel@gmail.com>
2024-06-24 20:29:29 +0100

    install/configure adonis/mail

--- start/env.ts
+++ start/env.ts
@@ -37,5 +37,13 @@ export default await Env.create(new URL('../', import.meta.url), {
   FACEBOOK_CLIENT_ID: Env.schema.string(),
   FACEBOOK_CLIENT_SECRET: Env.schema.string(),
   GOOGLE_CLIENT_ID: Env.schema.string(),
-  GOOGLE_CLIENT_SECRET: Env.schema.string()
+  GOOGLE_CLIENT_SECRET: Env.schema.string(),
+
+  /*
+  |----------------------------------------------------------
+  | Variables for configuring the mail package
+  |----------------------------------------------------------
+  */
+  SMTP_HOST: Env.schema.string(),
+  SMTP_PORT: Env.schema.string()
 })
