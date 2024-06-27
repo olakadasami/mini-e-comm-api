37038b5f2a02c956d7f63adc88fe9a243f6036c6
olakadasami <olakadasamuel@gmail.com>
2024-06-27 13:17:46 +0100

    add sending of verify email after registration

--- app/controllers/auth/register_controller.ts
+++ app/controllers/auth/register_controller.ts
@@ -1,8 +1,6 @@
 import type { HttpContext } from '@adonisjs/core/http'
-
 import User from '#models/user'
 import { registerValidator } from '#validators/auth'
-import mail from '@adonisjs/mail/services/main'
 
 export default class RegisterController {
   async handle({ request }: HttpContext) {
@@ -10,18 +8,10 @@ export default class RegisterController {
 
     const user = await User.create(userData)
 
-    const verificationToken = await User.emailVerificationTokens.create(user)
-    console.log(verificationToken)
-    const url = `http://localhost:3333/verify-email/${verificationToken.hash}`
-    await mail.send((message) => {
-      message
-        .to(user.email)
-        .subject('Verify your email address')
-        .htmlView('emails/verify', { user, url, token: verificationToken.hash })
-    })
+    await user.sendVerifyEmail()
 
-    // const token = await User.accessTokens.create(user)
+    const accessToken = await User.accessTokens.create(user)
 
-    return { message: 'Confirm your email to complete registration' }
+    return { message: 'User registered, Check mail to verify user account', token: accessToken }
   }
 }
